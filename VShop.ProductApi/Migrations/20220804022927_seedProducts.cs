e5fc774d12fe3b3e7faacb81b0a205492bb4f648
Eduardo Lima <eduardo.oliveira@legalcontrol.com.br>
2022-08-06 23:02:33 -0300

    Adicionar arquivos de projeto.

---
+++ VShop.ProductApi/Migrations/20220804022927_seedProducts.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.ProductApi.Migrations
+{
+    public partial class seedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImageURL, CategoryId)" +
+                "Values('Caderno', 7.55, 'Caderno 10 matérias', 10, 'caderno1.jpg', 1)");
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImageURL, CategoryId)" +
+                "Values('Lápis', 3.45, 'Lápis Preto', 10, 'Lápis.jpg', 1)");
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImageURL, CategoryId)" +
+                "Values('Luz RGB', 5.33, 'Luz RGB', 10, 'rgb1.jpg', 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Products");
+        }
+    }
+}
